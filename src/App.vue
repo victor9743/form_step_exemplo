3bb777b6889a69d68aebb10a3b811defd328b6b2
victor9743 <70709626+victor9743@users.noreply.github.com>
2021-08-16 16:13:17 +0100

    arquivos da pasta

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <stepForm />
+  </div>
+</template>
+
+<script>
+import stepForm from './components/stepForm.vue'
+export default {
+  name: 'App',
+  components: {
+    stepForm
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
